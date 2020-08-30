bc26463c54db8dae7d37dae9104b24f0c24b646f
Kelly Copley <copleykj@gmail.com>
2020-08-30 15:12:46 -0400

    update demo

--- demo/App.js
+++ demo/App.js
@@ -1,32 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Meteor, { Mongo, useTracker } from "@socialize/react-native-meteor";
+import { StyleSheet, Text, View, InteractionManager } from 'react-native';
+import Meteor, { Mongo, useTracker } from '@socialize/react-native-meteor';
+import NetInfo from '@react-native-community/netinfo';
+import Storage from '@react-native-community/async-storage';
+import { unstable_batchedUpdates as batchedUpdates } from 'react-native/Libraries/Renderer/shims/ReactNative';
 
-Meteor.connect("wss://atmospherejs.com/websocket");
-Meteor.subscribe("packages/recent");
+Meteor.configureOptionalDeps({ InteractionManager, batchedUpdates, NetInfo, Storage });
 
-const Packages = new Mongo.Collection("packages");
+Meteor.connect('wss://atmospherejs.com/websocket');
+Meteor.subscribe('packages/recent');
 
-export default function App() {
-  let packages = useTracker(() => {
-    return Packages.find({}, { sort: { "latestVersion.published": -1 } });
-  });
-  return (
-    <View style={styles.container}>
-      {packages.map(p => {
-        return <Text key={p._id}>{p.name}</Text>;
-      })}
-      <StatusBar style="auto" />
-    </View>
-  );
+const Packages = new Mongo.Collection('packages');
+
+export default function App () {
+    const packages = useTracker(() => {
+        return Packages.find({}, { sort: { 'latestVersion.published': -1 } });
+    });
+    return (
+        <View style={styles.container}>
+            {packages.map(p => {
+                return <Text key={p._id}>{p.name}</Text>;
+            })}
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
